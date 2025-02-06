58fa7d44a93ff1437786f56c940a31d035fc8dd1
Amer Mukhtar <amermukhtar19@gmail.com>
2025-02-07 00:21:00 +0500

    Upload

---
+++ lib/home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Scaffold(
+        body: Container(
+          child: ElevatedButton(onPressed: (){
+
+          }, child: Text("Send Notification")),
+        ),
+      ),
+    );
+  }
+}
